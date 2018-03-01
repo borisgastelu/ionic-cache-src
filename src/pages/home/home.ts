04b0af0635cbc5afbc85193e70ea874cfd19f614
borisgastelu <boris.gastelu@gmail.com>
2018-03-01 14:43:01 -0500

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,15 +9,22 @@ export class HomePage {
     segment = 'images';
 
     images = [
-        'https://html5box.com/html5gallery/images/Toronto_1024.jpg',
-        'http://wallpul.info/i/2016/12/wallpapers-wallpaper-full-hd-4k-xp-download-computer-hd-backgrounds.jpg',
-        'http://wallpul.info/i/2016/12/wallpapers-wallpaper-full-hd-4k-wallpapers-4k-ultra-hd-taringa-widescreen-desktop-backgrounds.jpg',
-        'http://wallpul.info/i/2016/12/wallpapers-wallpaper-full-hd-4k-full-hd-widescreen-wallpapers-hd.jpg'
+        'http://sample-videos.com/img/Sample-png-image-500kb.png',
+        'http://sample-videos.com/img/Sample-png-image-1mb.png',
+        'http://sample-videos.com/img/Sample-jpg-image-500kb.jpg',
+        'http://sample-videos.com/img/Sample-jpg-image-1mb.jpg'
     ];
 
     videos = [
-        // 'https://img-9gag-fun.9cache.com/photo/anMyBNV_460sv.mp4',
-        // 'https://img-9gag-fun.9cache.com/photo/aGj0GR6_460sv.mp4'
+        'http://sample-videos.com/video/mp4/240/big_buck_bunny_240p_2mb.mp4'
+    ];
+
+    audios = [
+        'http://sample-videos.com/audio/mp3/crowd-cheering.mp3'
+    ];
+
+    docs = [
+        'http://sample-videos.com/pdf/Sample-pdf-5mb.pdf'
     ];
 
 }
